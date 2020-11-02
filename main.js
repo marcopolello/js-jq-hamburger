453bc23b3f76f82159d0899ca145868fed41faf5
marcopolello <marco-pol@hotmail.it>
2020-11-02 15:38:22 +0100

    ex ok

---
+++ main.js
@@ -0,0 +1,26 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Partite come base del vostro esercizio dallo ZIP che vi passo:
+// Ricordate di scrivere solo il codice JavaScript con jQuery e non modificare HTML e CSS
+// Consiglio del giorno:
+// Partite dal comprendere bene come è fatto il DOM e il CSS, potete usarli a nostro vantaggio!
+
+//voglio che al click di <i> class="fas fa-bars" il div class="hamburger-menu diventi display block"
+
+var acceso = false;
+
+$('i.fas.fa-bars').click(function(){
+  if (acceso === true) {
+    $('.hamburger-menu').toggle('display') === 'block';
+  } else {
+    $('.hamburger-menu').toggle('display') === 'none';
+  }
+});
+$('i.fas.fa-times').click(function(){
+  if (acceso === true) {
+    $('.hamburger-menu').toggle('display') === 'none';
+  } else {
+    $('.hamburger-menu').toggle('display') === 'none';
+  }
+});
